cfec5216e634e7c722d5f09a8431e22783350b63
ZebGirouard <timzebgir@gmail.com>
2017-12-18 15:04:46 -0700

    Fixing create for wine and poke

--- controllers/pokemonCtrl.js
+++ controllers/pokemonCtrl.js
@@ -13,9 +13,8 @@ module.exports = {
   },
 
   create: (req,res) => {
-    console.log(req.body);
     var newPokemon = req.body;
-    newPokemon.create(newPokemon, function (err, savedPokemon) {
+    Pokemon.create(newPokemon, function (err, savedPokemon) {
       err ? res.status(500).json({ error: err.message }) :
         res.status(201).json(savedPokemon);
     });
